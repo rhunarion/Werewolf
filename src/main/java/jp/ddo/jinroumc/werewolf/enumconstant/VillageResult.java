97f3b45e2f660245cc5132148058ef8709591d2a
rhunarion <akira.kako@gmail.com>
2014-07-04 01:53:10 +0900

    delete

--- src/main/java/jp/ddo/jinroumc/werewolf/enumconstant/VillageResult.java
+++
@@ -1,8 +0,0 @@
-package jp.ddo.jinroumc.werewolf.enumconstant;
-
-public enum VillageResult {
-	draw,
-	murabito,
-	jinrou,
-	youko
-}
--- src/main/java/jp/ddo/jinroumc/werewolf/enumconstant/VillageStatus.java
+++
@@ -1,9 +0,0 @@
-package jp.ddo.jinroumc.werewolf.enumconstant;
-
-public enum VillageStatus {
-	empty,
-	preparing,
-	recruiting,
-	ongoing,
-	finishing
-}
