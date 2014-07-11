bf235da5e0ba121f26a87a26da08205b38677a3b
rhunarion <akira.kako@gmail.com>
2014-07-11 20:39:45 +0900

    remove ddo directory

--- Werewolf/src/main/java/jp/ddo/jinroumc/werewolf/enumconstant/VillageResult.java
+++
@@ -1,8 +0,0 @@
-package jp.ddo.jinroumc.werewolf.enumconstant;
-
-public enum VillageResult {
-	DRAW,
-	MURABITO,
-	JINROU,
-	YOUKO
-}
--- Werewolf/src/main/java/jp/ddo/jinroumc/werewolf/enumconstant/VillageStatus.java
+++
@@ -1,9 +0,0 @@
-package jp.ddo.jinroumc.werewolf.enumconstant;
-
-public enum VillageStatus {
-	EMPTY,
-	PREPARING,
-	RECRUITING,
-	ONGOING,
-	FINISHING
-}
